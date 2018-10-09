761598217cbbcf4fe9243f536cc501091b1f06f8
MuhiaKevin <archerkreiger@gmail.com>
2018-10-09 16:42:12 +0300

    can now save to a mongo database

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const monk = require('monk'); // will be used to save the data to mongodb
+
+// connect to the database
+const db  = monk('localhost/manudb');
+
+// get manuplayers collection
+const manuplayers = db.get('manuplayers');
+
+
+// inserting data into a mongodb
+
+function sendToMongo(playersobj){
+  let players = [];
+  players.push(playersobj);
+  manuplayers.insert(players);
+}
+
+
+
 
 
 
@@ -11,27 +30,30 @@ fs.readFile('players.json', 'utf8', function (err, data) {
 
     playerdata = JSON.parse(data);
 
-    // send all players
+  //   // send all players
+  //
+  //   app.get('/players',(request,response)=>{
+  //     response.send(playerdata)
+  //     console.log('Sent data to IP address => ' + request.ip)
+  //   });
+  //
+  //
+  // // sending players by positions
+  //
+  //   app.get('/players/:position',(request,response)=>{
+  //     let data = request.params
+  //     for(var i = 0 ; i <  Object.entries(playerdata).length; i++){
+  //     	if(data.position === Object.entries(playerdata)[i][0]){
+  //           // console.log(Object.entries(playerdata)[i][1]);
+  //           response.send(Object.entries(playerdata)[i][1]);
+  //           console.log('Sent data to IP address => ' + request.ip)
+  //     	}
+  //     }
+  //
+  //   });
 
-    app.get('/players',(request,response)=>{
-      response.send(playerdata)
-      console.log('Sent data to IP address => ' + request.ip)
-    });
+  sendToMongo(playerdata)
 
-
-  // sending players by positions
-
-    app.get('/players/:position',(request,response)=>{
-      let data = request.params
-      for(var i = 0 ; i <  Object.entries(playerdata).length; i++){
-      	if(data.position === Object.entries(playerdata)[i][0]){
-            // console.log(Object.entries(playerdata)[i][1]);
-            response.send(Object.entries(playerdata)[i][1]);
-            console.log('Sent data to IP address => ' + request.ip)
-      	}
-      }
-
-    });
 
 });
 
